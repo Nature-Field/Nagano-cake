085a42351d805ed5ceb2a6dcc065e6a3fbd00ad9
EC2 Default User <ec2-user@ip-172-31-89-132.ec2.internal>
2021-11-15 13:23:28 +0000

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,51 +2,66 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
+
+
+
+
+
+
+
+
+
+
   #customer側↓
-  
+
   #ホーム画面ルーティング
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-  #customerコントローラーのルーティング
-  resource :customers, only: [show, edit, update]
-  get 'customers/leave' => 'customers#leave'
-  patch 'customers/withdraw' => 'customers#withdraw'
-  
-  #productコントローラーのルーティング 
-  resources :products, only: [:index, :show]
-  
-  #addressコントローラーのルーティング
-  resources :addresses, except:[:show, :new]
-  
-  #orderコントローラーのルーティング
-  resources :orders, only: [:index, :show, :create, :new]
-  get 'orders/compleat' => 'orders#compleat'
-  post 'orders/confirm' => 'orders#confirm'
-  
-  #cartproductコントローラーのルーティング
-  resources :cart_products, only:[:index, :destroy, :edit, :update]
-  delete 'cart_products/destroy_all' => 'cart_products#destroy_all'
-  
+
+  scope module: :customer do
+
+    #カスタマー
+    resource :customers, only: [:show, :edit, :update]
+      get 'customers/leave' => 'customers#leave'
+      patch 'customers/withdraw' => 'customers#withdraw'
+
+    #商品
+    resources :products, only: [:index, :show]
+
+    #配送先
+    resources :addresses, only: [:index, :create, :edit, :update, :destroy]
+
+    #注文
+    resources :orders, only: [:index, :show, :create, :new]
+      get 'orders/compleat' => 'orders#compleat'
+      post 'orders/confirm' => 'orders#confirm'
+
+    #カート商品
+    resources :cart_products, only:[:index, :destroy, :edit, :update, :create]
+      delete 'cart_products/destroy_all' => 'cart_products#destroy_all'
+  end
+
   #admin側↓
-  
+
   namespace :admin do
     #トップページのルーティング
     get 'admin' => 'homes#top'
-    #customersコントローラーのルーティング
+    
+    #カスタマー
     resources :customers, only:[:index, :show, :edit, :update]
-    
-    #genresコントローラーのルーティング
+
+    #ジャンル
     resources :genres, only:[:index, :edit, :create, :update]
-    
-    #productsコントローラーのルーティング
+
+    #商品
     resources :products, except:[:destroy]
-    
-    #ordersコントローラーのルーティング
+
+    #注文
     resources :orders, only:[:show, :update]
-    
-    #order_detailsコントローラーのルーティング
+
+    #注文詳細
     resources :order_details, only:[:update]
   end
-  
+
 end
