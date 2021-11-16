3082d54194e1579a5be8f1d559fd6ee1741e4d82
Kyohey Kosugi <90119649+Rsnca@users.noreply.github.com>
2021-11-16 15:22:02 +0900

    Update cart_product.rb
    スペルミス修正

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -1,5 +1,5 @@
 class CartProduct < ApplicationRecord
-  validate :quantitiy, presence: true 
-  validate :product_id, presence: true
-  validate :customer_id, presence: true
+  validates :quantitiy, presence: true 
+  validates :product_id, presence: true
+  validates :customer_id, presence: true
 end
