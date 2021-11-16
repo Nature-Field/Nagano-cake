67bce03c64c5ee725b935bf1034355c5e9818efd
Kyohey Kosugi <90119649+Rsnca@users.noreply.github.com>
2021-11-16 15:29:03 +0900

    Update customer.rb
    customerバリデーション記載

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,14 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :phone_number, uniqueness: true, presence: true
+  validates :postal_code, presence: true, numericality: {only_integer:true}, length: {is: 7}
+  validates :adress, presence: true
+  validates :is_deleted, inclusion: {in:[true,false]}
 end
