11f7f7bc6241f6379d9ad44d57bc4a74bae91e51
kmikiya <91042669+kmikiya@users.noreply.github.com>
2021-11-16 15:01:18 +0900

    Update 20211116042033_create_products.rb

--- db/migrate/20211116042033_create_products.rb
+++ db/migrate/20211116042033_create_products.rb
@@ -2,12 +2,12 @@ class CreateProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :products do |t|
       
-      t.integer :genre_id
-      t.boolean :is_active
-      t.string  :name
-      t.text    :explanation
-      t.integer :price
-      t.string  :image_id
+      t.integer :genre_id,    null: false
+      t.boolean :is_active,   null: false, default: false
+      t.string  :name,        null: false
+      t.text    :explanation, null: false
+      t.integer :price,       null: false
+      t.string  :image_id,    null: false
       t.timestamps
     end
   end
