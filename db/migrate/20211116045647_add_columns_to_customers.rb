1b10f8139eca7d32c1f07013f184de4427854be9
kmikiya <k.mikiya97@gmail.com>
2021-11-16 08:09:51 +0000

    MVC完成

--- db/migrate/20211116045647_add_columns_to_customers.rb
+++ db/migrate/20211116045647_add_columns_to_customers.rb
@@ -1,13 +1,13 @@
 class AddColumnsToCustomers < ActiveRecord::Migration[5.0]
   def change
-    add_column :customers, :is_deleted,      :boolean, null: false, default: false
-    add_column :customers, :last_name,       :string,  null: false
-    add_column :customers, :first_name,      :string,  null: false
-    add_column :customers, :last_name_kana,  :string,  null: false
-    add_column :customers, :first_name_kana, :string,  null: false
-    add_column :customers, :phone_number,    :string,  null: false
-    add_column :customers, :postal_code,     :string,  null: false
-    add_column :customers, :address,         :string,  null: false
-    
+    add_column :customers, :is_deleted,      :boolean
+    add_column :customers, :last_name,       :string
+    add_column :customers, :first_name,      :string
+    add_column :customers, :last_name_kana,  :string
+    add_column :customers, :first_name_kana, :string
+    add_column :customers, :phone_number,    :string
+    add_column :customers, :postal_code,     :string
+    add_column :customers, :address,         :string
+
   end
 end
