c58f79238ec4ec6b6d94ee8380f8f42d7a77a8ec
nakayama91 <90545873+nakayama91@users.noreply.github.com>
2021-11-16 15:23:23 +0900

    Update shipping_address.rb

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,2 +1,11 @@
 class ShippingAddress < ApplicationRecord
+  
+  belongs_to :customer
+  
+  validate   :customer_id, presence: true
+  validate   :name, presence: true
+  validate   :postal_code, presence: ture, numericality: {only_integer: true}, length: {is: 7}
+  validate   :address, presence: true
+  
 end
+
