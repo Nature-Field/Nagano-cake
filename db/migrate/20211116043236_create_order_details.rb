3a52bc4fac3b387691c5557eb6a1505c02e9ac85
kmikiya <91042669+kmikiya@users.noreply.github.com>
2021-11-16 15:12:39 +0900

    Update 20211116043236_create_order_details.rb

--- db/migrate/20211116043236_create_order_details.rb
+++ db/migrate/20211116043236_create_order_details.rb
@@ -2,11 +2,11 @@ class CreateOrderDetails < ActiveRecord::Migration[5.0]
   def change
     create_table :order_details do |t|
 
-      t.integer :product_id
-      t.integer :order_id
-      t.integer :price
-      t.integer :quantity
-      t.integer :create_status
+      t.integer :product_id,     null: false
+      t.integer :order_id,       null: false
+      t.integer :price,          null: false
+      t.integer :quantity,       null: false
+      t.integer :create_status,  null: false
       t.timestamps
     end
   end
