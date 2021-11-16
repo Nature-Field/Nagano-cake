19765b5e9cbee6e9b061e4c8c64aeeed43677bb7
nakayama91 <90545873+nakayama91@users.noreply.github.com>
2021-11-16 15:31:45 +0900

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,16 @@
 class Product < ApplicationRecord
+  
+  belongs_to :genre
+  has_many   :cart_products
+  has_many   :order_detail
+  
+  validate :genre_id, presence: true
+  validate :is_active, inclusion: { in: [true, false] }
+  validate :name, presence: true
+  validate :explanation, presence: true
+  validate :price, presence: true
+  image_id, presence: true
+  
+  attachment :image_id
+  
 end
