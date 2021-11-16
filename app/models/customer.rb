1513d40fa3abe9a1c7d374ba9f675657fa70cde3
Kyohey Kosugi <90119649+Rsnca@users.noreply.github.com>
2021-11-16 15:36:59 +0900

    Update customer.rb
    アソシエーション追記

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,10 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  has_many :cart_products
+  has_many :orders
+  has_many :shipping_addresses
+  
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
