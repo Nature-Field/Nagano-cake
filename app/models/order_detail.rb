db356e13bd54b601733f3222d6092ed77a8aaa5c
nakayama91 <90545873+nakayama91@users.noreply.github.com>
2021-11-16 15:36:09 +0900

    Update order_detail.rb

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,12 @@
 class OrderDetail < ApplicationRecord
+  
+  belongs_to :product
+  belongs_to :order
+  
+  validates :product_id, presence: true
+  validates :oreder_id, presence: true
+  validates :price, presence: true
+  validates :quantity, presence: true
+  validates :create_status, presence: true
+  
 end
