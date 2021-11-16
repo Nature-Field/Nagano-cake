eca8a756231794126868ae3992edf7186b11a70c
nakayama91 <90545873+nakayama91@users.noreply.github.com>
2021-11-16 15:46:21 +0900

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,15 @@
 class Order < ApplicationRecord
+  
+  has_many   :order_detail
+  belongs_to :customer
+  
+  validates :customer_id, presence: true
+  validates :postal_code, presence: true, numericality: {only_integer: true}, length: {is: 7}
+  validates :address, presence: true
+  validates :name, presence: true
+  validates :cost, presence: true
+  validates :total_price, presence: true
+  validates :payment_way, presence: true
+  validates :order_status, presence: true
+  
 end
