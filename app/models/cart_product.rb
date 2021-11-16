a57fe2fe0d3afdadd12af801371dbb01fe1993af
Kyohey Kosugi <90119649+Rsnca@users.noreply.github.com>
2021-11-16 15:34:36 +0900

    Update cart_product.rb
    アソシエーション追記

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -1,4 +1,8 @@
 class CartProduct < ApplicationRecord
+  
+  belongs_to :customer
+  belongs_to :product
+  
   validates :quantitiy, presence: true 
   validates :product_id, presence: true
   validates :customer_id, presence: true
