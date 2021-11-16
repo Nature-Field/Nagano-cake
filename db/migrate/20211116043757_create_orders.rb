14c5e1164f9f1ddb9ba17181453365b43410b898
kmikiya <91042669+kmikiya@users.noreply.github.com>
2021-11-16 15:18:59 +0900

    Update 20211116043757_create_orders.rb

--- db/migrate/20211116043757_create_orders.rb
+++ db/migrate/20211116043757_create_orders.rb
@@ -2,14 +2,14 @@ class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
 
-      t.integer :customer_id
-      t.string  :postal_code
-      t.string  :address
-      t.string  :name
-      t.integer :cost
-      t.integer :total_price
-      t.integer :payment_way
-      t.integer :status
+      t.integer :customer_id,  null: false
+      t.string  :postal_code,  null: false
+      t.string  :address,      null: false
+      t.string  :name,         null: false
+      t.integer :cost,         null: false
+      t.integer :total_price,  null: false
+      t.integer :payment_way,  null: false, default: "0"
+      t.integer :status,       null: false, default: "0"
       t.timestamps
     end
   end
