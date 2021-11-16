7901a534dff8917a419d1b6718efc058b652d50c
kmikiya <k.mikiya97@gmail.com>
2021-11-16 04:42:57 +0000

    customer側のcontroller,view

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,19 @@
+class OrdersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def compleat
+  end
+
+  def new
+  end
+
+  def confirm
+  end
+end
