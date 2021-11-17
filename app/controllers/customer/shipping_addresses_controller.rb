6102dad46cb0a58215f48e4401176c398d015606
kmikiya <k.mikiya97@gmail.com>
2021-11-17 06:19:21 +0000

    アドレス一覧編集作成途中

--- app/controllers/customer/shipping_addresses_controller.rb
+++ app/controllers/customer/shipping_addresses_controller.rb
@@ -1,18 +1,35 @@
 class Customer::ShippingAddressesController < ApplicationController
-  
+
   def index
+    @shipping_address = ShippingAddress.new
+    @shipping_addresses = ShippingAddress.all
   end
 
   def create
+    @shipping_address = ShippingAddress.new(shipping_address_params)
+    redirect_to shipping_addresses_path
   end
 
   def edit
+    @shipping_address = ShippingAddress.find(params[:id])
   end
 
   def update
+    @shipping_address = ShippingAddress.find(params[:id])
+    @shipping_address.update(shipping_address_params)
+    redirect_to shipping_addresses_path
   end
 
   def destroy
+    shipping_address = ShippingAddress.find(params[:id])
+    shipping_address.destroy
+    redirect_to request.referer
   end
-  
+
+  private
+
+  def shipping_address_params
+    params.require(:shipping_address).permit(:postal_code, :address, :name)
+  end
+
 end
