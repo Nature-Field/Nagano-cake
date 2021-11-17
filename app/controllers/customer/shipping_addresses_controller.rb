4b8250ddc67f737cdc556d0cd5007bf2fbbae90e
kmikiya <k.mikiya97@gmail.com>
2021-11-17 06:49:52 +0000

    住所登録完成

--- app/controllers/customer/shipping_addresses_controller.rb
+++ app/controllers/customer/shipping_addresses_controller.rb
@@ -2,12 +2,17 @@ class Customer::ShippingAddressesController < ApplicationController
 
   def index
     @shipping_address = ShippingAddress.new
-    @shipping_addresses = ShippingAddress.all
+    @addresses = ShippingAddress.all
+
   end
 
   def create
     @shipping_address = ShippingAddress.new(shipping_address_params)
-    redirect_to shipping_addresses_path
+    if @shipping_address.save
+      redirect_to shipping_addresses_path, notice: "You have created book successfully."
+    else @addresses = ShippingAddress.all
+      render 'index'
+    end
   end
 
   def edit
@@ -16,8 +21,11 @@ class Customer::ShippingAddressesController < ApplicationController
 
   def update
     @shipping_address = ShippingAddress.find(params[:id])
-    @shipping_address.update(shipping_address_params)
-    redirect_to shipping_addresses_path
+    if @shipping_address.update(shipping_address_params)
+      redirect_to shipping_addresses_path
+    else
+      render "edit"
+    end
   end
 
   def destroy
@@ -29,7 +37,7 @@ class Customer::ShippingAddressesController < ApplicationController
   private
 
   def shipping_address_params
-    params.require(:shipping_address).permit(:postal_code, :address, :name)
+    params.require(:shipping_address).permit(:postal_code, :address, :name, :customer_id)
   end
 
 end
