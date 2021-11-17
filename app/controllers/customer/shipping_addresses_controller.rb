904ede5e4f192371178ecfaf0f153545cb1d99c6
kmikiya <k.mikiya97@gmail.com>
2021-11-17 11:18:09 +0000

    ショッピングカート実装途中

--- app/controllers/customer/shipping_addresses_controller.rb
+++ app/controllers/customer/shipping_addresses_controller.rb
@@ -36,8 +36,6 @@ class Customer::ShippingAddressesController < ApplicationController
 
   private
 
-  def shipping_address_params
-    params.require(:shipping_address).permit(:postal_code, :address, :name, :customer_id)
-  end
+@cart_product = CartProduct.new
 
 end
