2afe31579dd96513a0769846bcfa5bcfd199b6d9
mckee17 <hayate0173@gmail.com>
2021-11-17 07:54:02 +0000

    [add]product-new&index&show

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,16 +1,17 @@
 class Product < ApplicationRecord
-  
-  belongs_to :genre
+
+  #genre未実装のため、optional~追加：他modelがnilでもOK
+  belongs_to :genre, optional: true
   has_many   :cart_products
   has_many   :order_details
-  
+
   validates :genre_id, presence: true
   validates :is_active, inclusion: { in: [true, false] }
   validates :name, presence: true
   validates :explanation, presence: true
   validates :price, presence: true
-  validates :image_id, presence: true
-  
-  attachment :image_id
-  
+  validates :image, presence: true
+
+  attachment :image
+
 end
