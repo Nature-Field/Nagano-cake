f496ab9b2693872e67981f3ddab722100e6df115
kmikiya <k.mikiya97@gmail.com>
2021-11-18 01:53:31 +0000

    すべて削除以外完成

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -6,4 +6,9 @@ class CartProduct < ApplicationRecord
   validates :quantity, presence: true
   validates :product_id, presence: true
   validates :customer_id, presence: true
+
+  def sum_of_price
+    product.price * quantity
+  end
+
 end
