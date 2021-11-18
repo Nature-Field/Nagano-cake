e831da44d8b99fbc25b90b0a7c48d2a44509cdaf
EC2 Default User <ec2-user@ip-172-31-89-132.ec2.internal>
2021-11-18 05:13:26 +0000

    m

--- app/controllers/customer/homes_controller.rb
+++ app/controllers/customer/homes_controller.rb
@@ -1,9 +1,11 @@
 class Customer::HomesController < ApplicationController
-  
+
   def top
+    @product = Product.limit(4).order(created_at: :desc)
+    @image = @Product.image
   end
 
   def about
   end
-  
+
 end
