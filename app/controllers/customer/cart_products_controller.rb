a2455c34b6e24181d8f281bee8925bec14418c4b
kmikiya <k.mikiya97@gmail.com>
2021-11-18 05:02:21 +0000

    注文情報入力画面手詰まり

--- app/controllers/customer/cart_products_controller.rb
+++ app/controllers/customer/cart_products_controller.rb
@@ -13,6 +13,7 @@ class Customer::CartProductsController < ApplicationController
   end
 
   def destroy
+
     @cart_product = CartProduct.find(params[:id])
     @cart_product.destroy
     redirect_to cart_products_path
