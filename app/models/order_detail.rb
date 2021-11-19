d78a8101628951e26ba9037fc8aa338348b27200
mckee17 <hayate0173@gmail.com>
2021-11-19 05:31:13 +0000

    [fix]order-create&orderdetail-model

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -4,7 +4,7 @@ class OrderDetail < ApplicationRecord
   belongs_to :order
 
   validates :product_id, presence: true
-  validates :oreder_id, presence: true
+  validates :order_id, presence: true
   validates :price, presence: true
   validates :quantity, presence: true
   validates :create_status, presence: true
