bc43a65407d266bf3d23011b19791635470360f1
EC2 Default User <ec2-user@ip-172-31-89-132.ec2.internal>
2021-11-21 14:04:19 +0000

    ログイン者表示修正

--- app/controllers/customer/homes_controller.rb
+++ app/controllers/customer/homes_controller.rb
@@ -2,7 +2,6 @@ class Customer::HomesController < ApplicationController
 
   def top
     @product = Product.limit(4).order(created_at: :desc)
-    @customer_fullname = current_customer.fullname
   end
 
   def about
