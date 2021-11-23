7619d7700513a6151bf0fb696ccf4e589e8e4de7
EC2 Default User <ec2-user@ip-172-31-89-132.ec2.internal>
2021-11-23 10:11:17 +0000

    遷移先修正

--- app/controllers/customer/registrations_controller.rb
+++ app/controllers/customer/registrations_controller.rb
@@ -40,6 +40,10 @@ class Customer::RegistrationsController < Devise::RegistrationsController
 
   # protected
 
+  def after_sign_up_path_for(_resource)
+    customers_my_page_path
+  end
+
   def after_update_path_for(resource)
     customers_my_page_path
   end
